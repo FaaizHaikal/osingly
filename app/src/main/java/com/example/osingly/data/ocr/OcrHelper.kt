fded07ad50e37faf734e0cae61c975cc902afc74
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-21 22:49:17 +0700

    feat: ocr helper object

---
+++ app/src/main/java/com/example/osingly/data/ocr/OcrHelper.kt
@@ -0,0 +1,17 @@
+package com.example.osingly.data.ocr
+
+import android.graphics.Bitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlinx.coroutines.tasks.await
+
+object OcrHelper {
+    suspend fun extractTextFromImage(bitmap: Bitmap): String {
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val result = recognizer.process(image).await()
+
+        return result.text
+    }
+}

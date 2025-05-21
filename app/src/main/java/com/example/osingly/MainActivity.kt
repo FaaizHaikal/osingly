91eddc9ecaf6a541c7b7751669929a4f940d45f7
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-21 22:50:29 +0700

    feat: main activity

--- app/src/main/java/com/example/osingly/MainActivity.kt
+++ app/src/main/java/com/example/osingly/MainActivity.kt
@@ -3,45 +3,31 @@ package com.example.osingly
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.osingly.data.api.TranslationApi
+import com.example.osingly.data.ocr.OcrHelper
+import com.example.osingly.di.AppModule
+import com.example.osingly.ui.OcrScreen
 import com.example.osingly.ui.theme.OsinglyTheme
+import com.example.osingly.viewmodel.OcrViewModel
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val viewModel = AppModule.provideOcrViewModel()
+
         setContent {
             OsinglyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                OcrScreen(
+                    viewModel = viewModel,
+                    onResult = { result ->
+                        println("Translation result: $result")
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OsinglyTheme {
-        Greeting("Android")
     }
 }

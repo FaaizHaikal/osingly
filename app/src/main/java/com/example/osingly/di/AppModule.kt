cf6a6dc65aea0a45acc710c5d34ea64f2f9f6ad8
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-11 23:35:54 +0700

    feat: translation view model di

--- app/src/main/java/com/example/osingly/di/AppModule.kt
+++ app/src/main/java/com/example/osingly/di/AppModule.kt
@@ -1,23 +1,28 @@
 package com.example.osingly.di
 
-import com.example.osingly.data.api.TranslationApi
-import com.example.osingly.data.ocr.OcrHelper
+import com.example.osingly.data.api.TranslationService
+import com.example.osingly.data.api.OcrService
 import com.example.osingly.viewmodel.OcrViewModel
+import com.example.osingly.viewmodel.TranslationViewModel
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AppModule {
-    val translationApi: TranslationApi by lazy {
+    val translationService: TranslationService by lazy {
         Retrofit.Builder()
             .baseUrl("http://your.server.com/") // TODO: replace with real endpoint
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(TranslationApi::class.java)
+            .create(TranslationService::class.java)
     }
 
-    val ocrHelper = OcrHelper
+    val ocrService = OcrService
 
     fun provideOcrViewModel(): OcrViewModel {
-        return OcrViewModel(ocrHelper, translationApi)
+        return OcrViewModel(ocrService)
+    }
+
+    fun provideTranslationViewModel(): TranslationViewModel {
+        return TranslationViewModel(translationService)
     }
 }

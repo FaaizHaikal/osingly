ca5d070912f75397f075edcfee7c75f099eaba28
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-12 14:29:29 +0700

    refactor: remove unused

--- app/src/main/java/com/example/osingly/ui/theme/Theme.kt
+++ app/src/main/java/com/example/osingly/ui/theme/Theme.kt
@@ -1,49 +1,11 @@
 package com.example.osingly.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-
-// Dark theme colors
-//private val DarkColorScheme = darkColorScheme(
-//    primary = OceanBlue,
-//    secondary = SunsetOrange,
-//    tertiary = ForestGreen,
-//    background = Color(0xFF121212),
-//    surface = Color(0xFF1E1E1E),
-//    onPrimary = Color.White,
-//    onSecondary = Color.Black,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFFE0E0E0),
-//    onSurface = Color(0xFFE0E0E0),
-//    surfaceVariant = Color(0xFF2D2D2D),
-//    error = CoralPink
-//)
-//
-//// Light theme colors
-//private val LightColorScheme = lightColorScheme(
-//    primary = OceanBlue,
-//    secondary = GoldYellow,
-//    tertiary = ForestGreen,
-//    background = Color(0xFFF5F5F5),
-//    surface = Color.White,
-//    onPrimary = Color.White,
-//    onSecondary = Color.Black,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFF333333),
-//    onSurface = Color(0xFF333333),
-//    surfaceVariant = Color(0xFFEEEEEE),
-//    error = CoralPink
-//)
 
 private val LightColorScheme = lightColorScheme(
     primary = Color(0xFF446DF6),             // soft blue
@@ -100,7 +62,6 @@ val DarkColorScheme = darkColorScheme(
 @Composable
 fun OsinglyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false, // Disabled for custom colors
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
